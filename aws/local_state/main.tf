c5a5175ed4090a67bd13f60cb5651b734baa0739
borseatul007 <105062145+borseatul007@users.noreply.github.com>
2023-07-18 10:46:19 +0530

    Update main.tf

--- aws/local_state/main.tf
+++ aws/local_state/main.tf
@@ -10,15 +10,51 @@ terraform {
 }
 
 provider "aws" {
-  region  = "us-west-2"
+  region  = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = "ami-830c94e3"
-  instance_type = "t2.micro"
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "Terraform_Demo"
+    Name = "MyVPC"
   }
 }
 
+resource "aws_subnet" "public_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "PublicSubnet"
+  }
+}
+
+resource "aws_subnet" "private_subnet" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+
+  tags = {
+    Name = "PrivateSubnet"
+  }
+}
+
+resource "aws_instance" "public_instance" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.public_subnet.id
+
+  tags = {
+    Name = "PublicInstance"
+  }
+}
+
+resource "aws_instance" "private_instance" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.private_subnet.id
+
+  tags = {
+    Name = "PrivateInstance"
+  }
+}
